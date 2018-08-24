1dc00a1e66b26084a7f29a1550d77f48f6ead322
teckeon <teckeon@outlook.com>
2018-08-24 15:30:50 -0500

    edX update

---
+++ edX_SetOperators.R
@@ -0,0 +1,1056 @@
+library(tidyverse)		
+			
+
+	
+Addins
+
+
+	
+Project: (None)
+
+Environment
+History
+Connections
+
+			
+
+			
+	
+	
+Import Dataset
+
+	
+			
+
+	
+List
+
+	
+			
+
+	
+Global Environment
+
+
+Values
+converted	chr [1:5] "5'3" "5'5" "6'1" "5'11" "5'12"
+df1	chr [1:6] "x" "y" "a" "a" "b" "a"
+df2	chr [1:6] "x" "y" "a" "a" "a" "b"
+final	chr [1:4] "x" "y" "a" "b"
+final2	character (empty)
+final3	character (empty)
+final4	chr [1:4] "x" "y" "a" "b"
+index	logi [1:5] TRUE TRUE TRUE TRUE TRUE
+pattern	"^[4-7]\\s*'\\s*\\d{1,2}$"
+pattern_with_groups	"^([4-7])[,\\.\\s](\\d*)$"
+problems	chr [1:5] "5.3" "5,5" "6 1" "5 .11" "5, 12"
+Files
+Plots
+Packages
+Help
+Viewer
+
+	
+Install
+	
+	
+Update
+		
+
+	
+			
+Name	Description	Version	
+User Library
+
+assertthat
+Easy Pre and Post Assertions
+0.2.0
+
+
+backports
+Reimplementations of Functions Introduced Since R-3.0.0
+1.1.2
+
+
+base64enc
+Tools for base64 encoding
+0.1-3
+
+
+BH
+Boost C++ Header Files
+1.65.0-1
+
+
+bindr
+Parametrized Active Bindings
+0.1
+
+
+bindrcpp
+An 'Rcpp' Interface to Active Bindings
+0.2
+
+
+bit
+A class for vectors of 1-bit booleans
+1.1-12
+
+
+bit64
+A S3 Class for Vectors of 64bit Integers
+0.9-7
+
+
+bitops
+Bitwise Operations
+1.0-6
+
+
+blob
+A Simple S3 Class for Representing Vectors of Binary Data ('BLOBS')
+1.1.0
+
+
+boot
+Bootstrap Functions (Originally by Angelo Canty for S)
+1.3-20
+
+
+broom
+Convert Statistical Analysis Objects into Tidy Data Frames
+0.4.3
+
+
+callr
+Call R from R
+1.0.0
+
+
+caret
+Classification and Regression Training
+6.0-78
+
+
+caTools
+Tools: moving window statistics, GIF, Base64, ROC AUC, etc.
+1.17.1
+
+
+cellranger
+Translate Spreadsheet Cell Ranges to Rows and Columns
+1.1.0
+
+
+checkpoint
+Install Packages from Snapshots on the Checkpoint Server for Reproducibility
+0.4.3
+
+
+class
+Functions for Classification
+7.3-14
+
+
+cli
+Helpers for Developing Command Line Interfaces
+1.0.0
+
+
+clipr
+Read and Write from the System Clipboard
+0.4.0
+
+
+cluster
+"Finding Groups in Data": Cluster Analysis Extended Rousseeuw et al.
+2.0.6
+
+
+codetools
+Code Analysis Tools for R
+0.2-15
+
+
+colorspace
+Color Space Manipulation
+1.3-2
+
+
+compiler
+The R Compiler Package
+3.4.3
+
+
+config
+Manage Environment Specific Configuration Values
+0.2
+
+
+crayon
+Colored Terminal Output
+1.3.4
+
+
+curl
+A Modern and Flexible Web Client for R
+3.1
+
+
+CVST
+Fast Cross-Validation via Sequential Testing
+0.2-1
+
+
+data.table
+Extension of `data.frame`
+1.10.4-3
+
+
+datasets
+The R Datasets Package
+3.4.3
+
+
+DBI
+R Database Interface
+0.7
+
+
+dbplyr
+A 'dplyr' Back End for Databases
+1.1.0
+
+
+ddalpha
+Depth-Based Classification and Calculation of Data Depth
+1.3.1
+
+
+DEoptimR
+Differential Evolution Optimization in Pure R
+1.0-8
+
+
+deployrRserve
+Binary R server
+9.0.0
+
+
+dichromat
+Color Schemes for Dichromats
+2.0-0
+
+
+digest
+Create Compact Hash Digests of R Objects
+0.6.13
+
+
+dimRed
+A Framework for Dimensionality Reduction
+0.1.0
+
+
+doParallel
+Foreach Parallel Adaptor for the 'parallel' Package
+1.0.12
+
+
+dplyr
+A Grammar of Data Manipulation
+0.7.4
+
+
+DRR
+Dimensionality Reduction via Regression
+0.0.2
+
+
+e1071
+Misc Functions of the Department of Statistics, Probability Theory Group (Formerly: E1071), TU Wien
+1.6-8
+
+
+ElemStatLearn
+Data Sets, Functions and Examples from the Book: "The Elements of Statistical Learning, Data Mining, Inference, and Prediction" by Trevor Hastie, Robert Tibshirani and Jerome Friedman
+2015.6.26
+
+
+evaluate
+Parsing and Evaluation Tools that Provide More Details than the Default
+0.10.1
+
+
+forcats
+Tools for Working with Categorical Variables (Factors)
+0.2.0
+
+
+foreach
+Provides Foreach Looping Construct for R
+1.4.5
+
+
+foreign
+Read Data Stored by 'Minitab', 'S', 'SAS', 'SPSS', 'Stata', 'Systat', 'Weka', 'dBase', ...
+0.8-69
+
+
+formatR
+Format R Code Automatically
+1.5
+
+
+ggplot2
+Create Elegant Data Visualisations Using the Grammar of Graphics
+2.2.1
+
+
+glmnet
+Lasso and Elastic-Net Regularized Generalized Linear Models
+2.0-13
+
+
+glue
+Interpreted String Literals
+1.2.0
+
+
+gower
+Gower's Distance
+0.1.2
+
+
+graphics
+The R Graphics Package
+3.4.3
+
+
+grDevices
+The R Graphics Devices and Support for Colours and Fonts
+3.4.3
+
+
+grid
+The Grid Graphics Package
+3.4.3
+
+
+gtable
+Arrange 'Grobs' in Tables
+0.2.0
+
+
+haven
+Import and Export 'SPSS', 'Stata' and 'SAS' Files
+1.1.0
+
+
+hexbin
+Hexagonal Binning Routines
+1.27.1
+
+
+highr
+Syntax Highlighting for R Source Code
+0.6
+
+
+hms
+Pretty Time of Day
+0.4.0
+
+
+htmltools
+Tools for HTML
+0.3.6
+
+
+htmlwidgets
+HTML Widgets for R
+0.9
+
+
+httpuv
+HTTP and WebSocket Server Library
+1.3.5
+
+
+httr
+Tools for Working with URLs and HTTP
+1.3.1
+
+
+ipred
+Improved Predictors
+0.9-6
+
+
+IRdisplay
+'Jupyter' Display Machinery
+0.4.4
+
+
+IRkernel
+Native R Kernel for the 'Jupyter Notebook'
+0.8.11
+
+
+iterators
+Provides Iterator Construct for R
+1.0.9
+
+
+jsonlite
+A Robust, High Performance JSON Parser and Generator for R
+1.5
+
+
+kernlab
+Kernel-Based Machine Learning Lab
+0.9-25
+
+
+KernSmooth
+Functions for Kernel Smoothing Supporting Wand & Jones (1995)
+2.23-15
+
+
+knitr
+A General-Purpose Package for Dynamic Report Generation in R
+1.18
+
+
+labeling
+Axis Labeling
+0.3
+
+
+lattice
+Trellis Graphics for R
+0.20-35
+
+
+lava
+Latent Variable Models
+1.5.1
+
+
+lazyeval
+Lazy (Non-Standard) Evaluation
+0.2.1
+
+
+lubridate
+Make Dealing with Dates a Little Easier
+1.7.1
+
+
+magrittr
+A Forward-Pipe Operator for R
+1.5
+
+
+maps
+Draw Geographical Maps
+3.2.0
+
+
+markdown
+'Markdown' Rendering for R
+0.8
+
+
+MASS
+Support Functions and Datasets for Venables and Ripley's MASS
+7.3-47
+
+
+Matrix
+Sparse and Dense Matrix Classes and Methods
+1.2-12
+
+
+methods
+Formal Methods and Classes
+3.4.3
+
+
+mgcv
+Mixed GAM Computation Vehicle with Automatic Smoothness Estimation
+1.8-22
+
+
+MicrosoftR
+Microsoft R umbrella package
+3.4.3.0097
+
+
+mime
+Map Filenames to MIME Types
+0.5
+
+
+miniUI
+Shiny UI Widgets for Small Screens
+0.1.1
+
+
+mnormt
+The Multivariate Normal and t Distributions
+1.5-5
+
+
+ModelMetrics
+Rapid Calculation of Model Metrics
+1.1.0
+
+
+modelr
+Modelling Functions that Work with the Pipe
+0.1.1
+
+
+mongolite
+Fast and Simple 'MongoDB' Client for R
+1.4
+
+
+munsell
+Utilities for Using Munsell Colours
+0.4.3
+
+
+nlme
+Linear and Nonlinear Mixed Effects Models
+3.1-131
+
+
+nnet
+Feed-Forward Neural Networks and Multinomial Log-Linear Models
+7.3-12
+
+
+numDeriv
+Accurate Numerical Derivatives
+2016.8-1
+
+
+odbc
+Connect to ODBC Compatible Databases (using the DBI Interface)
+1.1.3
+
+
+openssl
+Toolkit for Encryption, Signatures and Certificates Based on OpenSSL
+0.9.9
+
+
+packrat
+A Dependency Management System for Projects and their R Package Dependencies
+0.4.8-1
+
+
+parallel
+Support for Parallel computation in R
+3.4.3
+
+
+pbdZMQ
+Programming with Big Data -- Interface to ZeroMQ
+0.2-6
+
+
+pillar
+Coloured Formatting for Columns
+1.0.1
+
+
+pkgconfig
+Private Configuration for 'R' Packages
+2.0.1
+
+
+PKI
+Public Key Infrastucture for R Based on the X.509 Standard
+0.1-5.1
+
+
+plogr
+The 'plog' C++ Logging Library
+0.1-1
+
+
+plyr
+Tools for Splitting, Applying and Combining Data
+1.8.4
+
+
+png
+Read and write PNG images
+0.1-7
+
+
+prodlim
+Product-Limit Estimation for Censored Event History Analysis
+1.6.1
+
+
+profvis
+Interactive Visualizations for Profiling R Code
+0.3.4
+
+
+psych
+Procedures for Psychological, Psychometric, and Personality Research
+1.7.8
+
+
+purrr
+Functional Programming Tools
+0.2.4
+
+
+quantmod
+Quantitative Financial Modelling Framework
+0.4-12
+
+
+R6
+Classes with Reference Semantics
+2.2.2
+
+
+randomForest
+Breiman and Cutler's Random Forests for Classification and Regression
+4.6-12
+
+
+rappdirs
+Application Directories: Determine Where to Save Data, Caches, and Logs
+0.3.1
+
+
+rbokeh
+R Interface for Bokeh
+0.6.3
+
+
+RColorBrewer
+ColorBrewer Palettes
+1.1-2
+
+
+Rcpp
+Seamless R and C++ Integration
+0.12.14
+
+
+RcppRoll
+Efficient Rolling / Windowed Operations
+0.2.2
+
+
+RCurl
+General Network (HTTP/FTP/...) Client Interface for R
+1.95-4.9
+
+
+readr
+Read Rectangular Text Data
+1.1.1
+
+
+readxl
+Read Excel Files
+1.0.0
+
+
+recipes
+Preprocessing Tools to Create Design Matrices
+0.1.1
+
+
+rematch
+Match Regular Expressions with a Nicer 'API'
+1.0.1
+
+
+repr
+Serializable Representations
+0.12.0
+
+
+reprex
+Prepare Reproducible Example Code for Sharing
+0.1.1
+
+
+reshape2
+Flexibly Reshape Data: A Reboot of the Reshape Package
+1.4.3
+
+
+RevoIOQ
+Microsoft R Services Test Suite
+8.0.9
+
+
+RevoMods
+R Functions Modified For Revolution R
+11.0.0
+
+
+RevoUtils
+Microsoft R Utility Package
+10.0.8
+
+
+RevoUtilsMath
+Microsoft R Services Math Utilities Package
+10.0.1
+
+
+rJava
+Low-Level R to Java Interface
+0.9-9
+
+
+RJDBC
+Provides access to databases through the JDBC interface
+0.2-5
+
+
+RJSONIO
+Serialize R objects to JSON, JavaScript Object Notation
+1.3-0
+
+
+rlang
+Functions for Base Types and Core R and 'Tidyverse' Features
+0.1.6
+
+
+rmarkdown
+Dynamic Documents for R
+1.8
+
+
+robustbase
+Basic Robust Statistics
+0.92-8
+
+
+rpart
+Recursive Partitioning and Regression Trees
+4.1-11
+
+
+rprojroot
+Finding Files in Project Subdirectories
+1.3-1
+
+
+rsconnect
+Deployment Interface for R Markdown Documents and Shiny Applications
+0.8.5
+
+
+rstudioapi
+Safely Access the RStudio API
+0.7
+
+
+RUnit
+R Unit test framework
+0.4.26
+
+
+rvest
+Easily Harvest (Scrape) Web Pages
+0.3.2
+
+
+scales
+Scale Functions for Visualization
+0.5.0
+
+
+selectr
+Translate CSS Selectors to XPath Expressions
+0.3-1
+
+
+sfsmisc
+Utilities from 'Seminar fuer Statistik' ETH Zurich
+1.1-1
+
+
+shiny
+Web Application Framework for R
+1.0.5
+
+
+sourcetools
+Tools for Reading, Tokenizing and Parsing R Code
+0.1.6
+
+
+sparklyr
+R Interface to Apache Spark
+0.6.4
+
+
+spatial
+Functions for Kriging and Point Pattern Analysis
+7.3-11
+
+
+splines
+Regression Spline Functions and Classes
+3.4.3
+
+
+stats
+The R Stats Package
+3.4.3
+
+
+stats4
+Statistical Functions using S4 Classes
+3.4.3
+
+
+stringi
+Character String Processing Facilities
+1.1.6
+
+
+stringr
+Simple, Consistent Wrappers for Common String Operations
+1.2.0
+
+
+survival
+Survival Analysis
+2.41-3
+
+
+tcltk
+Tcl/Tk Interface
+3.4.3
+
+
+tibble
+Simple Data Frames
+1.4.1
+
+
+tidyr
+Easily Tidy Data with 'spread()' and 'gather()' Functions
+0.7.2
+
+
+tidyselect
+Select from a Set of Strings
+0.2.3
+
+
+tidyverse
+Easily Install and Load the 'Tidyverse'
+1.2.1
+
+
+timeDate
+Rmetrics - Chronological and Calendar Objects
+3042.101
+
+
+tools
+Tools for Package Development
+3.4.3
+
+
+TTR
+Technical Trading Rules
+0.23-2
+
+
+utf8
+Unicode Text Processing
+1.1.2
+
+
+utils
+The R Utils Package
+3.4.3
+
+
+uuid
+Tools for generating and handling of UUIDs
+0.1-2
+
+
+viridisLite
+Default Color Maps from 'matplotlib' (Lite Version)
+0.2.0
+
+
+whisker
+{{mustache}} for R, logicless templating
+0.3-2
+
+
+withr
+Run Code 'With' Temporarily Modified Global State
+2.1.1
+
+
+xml2
+Parse XML
+1.1.1
+
+
+xtable
+Export Tables to LaTeX or HTML
+1.8-2
+
+
+xts
+eXtensible Time Series
+0.10-1
+
+
+yaml
+Methods to Convert R Data to YAML and Back
+2.1.16
+
+
+zoo
+S3 Infrastructure for Regular and Irregular Time Series (Z's Ordered Observations)
+1.8-0
+
+	
+edX_SetOperators.R
+*	
+
+			
+
+			
+	
+			
+	
+			
+	
+Source on Save
+		
+			
+
+			
+	
+			
+
+	
+Run
+	
+			
+	
+	
+Source
+
+			
+	
+			
+35:1
+(Top Level)
+R Script
+No outline available
+
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+# another section 
+converted <- problems %>% 
+  str_replace("feet|foot|ft", "'") %>% 
+  str_replace("inches|in|''|\"", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+index <- str_detect(converted, pattern)
+index
+converted[!index]
+Console~/
+Console
+Terminal
+
+~/	
+			
+
+			
+
+
+# df1 <- ( c("x","y"), c("a","a"), c("b","a") )
+# df2 <-  ( c("x","y"), c("a","a"), c("a","a") )
+
+
+df1 <- (c("x","y","a","a","b","a"))
+df2 <- (c("x","y","a","a","a","b"))
+df1
+df2
+final <- union(df1, df2)
+final2 <- setdiff(df1, df2)
+final3 <- setdiff(df2, df1)
+final4 <- intersect(df1, df2)
+
+final
+final2
+final3
+final4
+
+# additional questions
+problems <- c("5.3","5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.\\s](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+# another section 
+converted <- problems %>% 
+  str_replace("feet|foot|ft", "'") %>% 
+  str_replace("inches|in|''|\"", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+index <- str_detect(converted, pattern)
+index
+converted[!index]
+
+# another problem
+
+yes <- c("5 feet 7inches", "5 7")
+no <- c("5ft 9 inches", "5 ft 9 inches")
+s <- c(yes, no)
+
+converted <- s %>% 
+  str_replace("\\s*(feet|foot|ft)\\s*", "'") %>% 
+  str_replace("\\s*(inches|in|''|\")\\s*", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+str_detect(converted, pattern)
+
+
+
+
+
